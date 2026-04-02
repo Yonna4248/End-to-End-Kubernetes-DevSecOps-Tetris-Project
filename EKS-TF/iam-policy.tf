b865c2e10fce2122d0a8c7c96d0a0413f3376d4b
Chayanon Puranapura - 6609650251 <145308866+Yonna4248@users.noreply.github.com>
2026-04-02 10:07:15 +0700

    fixerror

--- EKS-TF/iam-policy.tf
+++ EKS-TF/iam-policy.tf
@@ -1,19 +1,1 @@
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = data.aws_iam_role.EKSClusterRole.name
-}
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = data.aws_iam_role.NodeGroupRole.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = data.aws_iam_role.NodeGroupRole.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = data.aws_iam_role.NodeGroupRole.name
-}
