72614d0e46dc16939f8626022c285c4b469338d2
Chayanon Puranapura - 6609650251 <145308866+Yonna4248@users.noreply.github.com>
2026-04-02 09:35:07 +0700

    labrole

--- EKS-TF/iam-role.tf
+++ EKS-TF/iam-role.tf
@@ -1,31 +1,7 @@
-resource "aws_iam_role" "EKSClusterRole" {
-  name = "EKSClusterRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-    ]
-  })
+data "aws_iam_role" "EKSClusterRole" {
+  name = "LabRole"
 }
 
-resource "aws_iam_role" "NodeGroupRole" {
-  name = "EKSNodeGroupRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
+data "aws_iam_role" "NodeGroupRole" {
+  name = "LabRole"
 }
